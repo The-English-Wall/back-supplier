36801381cfee91d6a194cb7c18fc88a996064c22
ARojasPolanco <advbrrop23@gmail.com>
2024-05-16 19:26:26 -0300

    fix: change axios config

--- src/config/conections/axios.config.js
+++ src/config/conections/axios.config.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+import { envs } from "../enviroments/enviroments.js";
 
 export const BASE_URL_COMPANY = axios.create({
-    baseURL: 'http://localhost:3000/api/v1'
+    baseURL: `http://${envs.DB_HOST}:${envs.DB_PORT}/api/v1/}`
 })
 
