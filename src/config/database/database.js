b094229b326c897698affad57d2919f9ce9c88b6
ARojasPolanco <advbrrop23@gmail.com>
2024-05-16 18:49:01 -0300

    fix: change enviroments and change config axios

--- src/config/database/database.js
+++ src/config/database/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 import { envs } from '../enviroments/enviroments.js';
 
-export const sequelize = new Sequelize(`postgresql://${envs.DB_USERNAME_SUPPLIER}:${envs.DB_PASSWORD_SUPPLIER}@${envs.DB_HOST_SUPPLIER}:${envs.DB_PORT}/${envs.DB_NAME_SUPPLIER}`, {
+export const sequelize = new Sequelize(`postgresql://${envs.DB_USERNAME}:${envs.DB_PASSWORD}@${envs.DB_HOST}:${envs.DB_PORT}/${envs.DB_NAME_SUPPLIER}`, {
   logging: false,
   ssl: true
 });
@@ -17,7 +17,7 @@ export async function authenticate() {
 
 export async function sincronize() {
   try {
-    await sequelize.sync({ force: true });
+    await sequelize.sync();
     console.log('db Sync ok!');
   } catch (error) {
     throw new Error('Synchronization error');
