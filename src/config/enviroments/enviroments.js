480932074398308de845a387fe88f3ad988489ee
ARojasPolanco <advbrrop23@gmail.com>
2024-05-16 20:10:42 -0300

    fix: change axios config

--- src/config/enviroments/enviroments.js
+++ src/config/enviroments/enviroments.js
@@ -4,6 +4,7 @@ import env from "env-var"
 
 export const envs = {
     PORT_SUPPLIER: env.get("PORT_SUPPLIER").required().asPortNumber(),
+    PORT_COMPANY: env.get("PORT_COMPANY").required().asPortNumber(),
     NODE_ENV: env.get("NODE_ENV").required().asString(),
     // DB_URI: env.get("DB_URI").required().asString(),
     SECRET_JWD_SEED: env.get("SECRET_JWD_SEED").required().asString(),
