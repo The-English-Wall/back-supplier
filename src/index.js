dd2bbf89fd1b9ab7829e43fa4aa7c93ec385efeb
ARojasPolanco <advbrrop23@gmail.com>
2024-05-23 19:11:31 -0300

    feat: force true changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import app from "./app.js";
 import { envs } from "./config/enviroments/enviroments.js";
 import { authenticate, sincronize } from "./config/database/database.js"
 import { initModel } from "./config/database/associations.js";
+import { BASE_URL_COMPANY } from "./config/conections/axios.config.js";
+import axios from "axios";
 
 async function main() {
     try {
@@ -15,7 +17,19 @@ async function main() {
 
 main()
 
-
 app.listen(envs.PORT_SUPPLIER, () => {
     console.log(`Server running on ${envs.PORT_SUPPLIER}`);
 });
+
+// app.get('/companyValidation', async (req, res) => {
+//     try {
+//         const response = await axios.get(`/http://${envs.DB_HOST}:${envs.PORT_COMPANY}/api/v1`);
+//         res.json(response.data);
+//     } catch (error) {
+//         res.status(500).json({ error: 'Failed to fetch data from Microservice A' });
+//     }
+// });
+
+// app.listen(envs.PORT_SUPPLIER, () => {
+//     console.log(`Microservice B listening on port ${envs.PORT_SUPPLIER}`);
+// });
